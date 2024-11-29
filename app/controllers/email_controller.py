25b4f8f28120a75dd4193f648406c5d9548779eb
Louiz77 <luizgu009@gmail.com>
2024-11-29 14:48:45 -0300

    fix: log email controller

--- app/controllers/email_controller.py
+++ app/controllers/email_controller.py
@@ -1,14 +1,11 @@
 from flask import jsonify
 from app.models.email_service import EmailService
-from datetime import datetime
 
 def check_email():
+    email_service = EmailService()
     try:
         email_service = EmailService()
         resultado = email_service.process_emails()
-        print(resultado)
-        with open("report.log", "a") as my_file:
-            my_file.write(f"-{datetime.now()} | Error: {resultado}\n")
         emails = email_service.fetch_emails_from_sender()
         if not emails:
             return jsonify({"message": "Nenhum novo e-mail encontrado."}), 404
