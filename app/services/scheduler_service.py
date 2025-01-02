399311b153f5c4a11b6926e35b8edeb5572a0df2
Louiz77 <luizgu009@gmail.com>
2025-01-02 10:38:14 -0300

    scheduler_service.py update | timezone configuration

--- app/services/scheduler_service.py
+++ app/services/scheduler_service.py
@@ -1,6 +1,10 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from app.models.email_service import EmailService
 from app.services.registrar_log import logger
+from pytz import timezone
+import os
+
+os.environ['TZ'] = 'America/Sao_Paulo'
 
 def atualizar_planilha_automaticamente():
     """
@@ -21,18 +25,21 @@ def schedule_planilha():
         func=atualizar_planilha_automaticamente,
         trigger="cron",
         hour=15,         # Hora de execução
-        minute=2         # Minuto de execução
+        minute=2,         # Minuto de execução
+        timezone = timezone('America/Sao_Paulo')
     )
     scheduler.add_job(
         func=atualizar_planilha_automaticamente,
         trigger="cron",
         hour=15,         # Hora de execução
-        minute=35         # Minuto de execução
+        minute=35,         # Minuto de execução
+        timezone=timezone('America/Sao_Paulo')
     )
     scheduler.add_job(
         func=atualizar_planilha_automaticamente,
         trigger="cron",
         hour=16,         # Hora de execução
-        minute=30         # Minuto de execução
+        minute=30,         # Minuto de execução
+        timezone=timezone('America/Sao_Paulo')
     )
     scheduler.start()
